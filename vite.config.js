0603fb8d0fd6f17bce8be61bad9986bc80c33f7e
LTNhoTin <tinle155@gmail.com>
2024-07-26 19:14:23 +0700

    :-1:

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/phunubinhduong/', // Cập nhật base URL cho GitHub Pages
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    host: true
-  }
+  base: '/phunubinhduong/', 
 });
