96cfd75a05e785c28319f8587263fa0bb79ba182
LTNhoTin <tinle155@gmail.com>
2024-07-26 18:48:41 +0700

    u

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/phunubinhduong/', 
+  base: '/phunubinhduong/', // Thay đổi base thành tên kho lưu trữ của bạn
+  build: {
+    outDir: 'dist'
+  }
 });
